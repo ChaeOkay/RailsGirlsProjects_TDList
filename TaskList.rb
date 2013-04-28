c88bc0da286b53254886dfe259ee106ce0536208
cokeefe <chaemobile@gmail.com>
2013-04-28 19:37:32 -0400

    add CRUD structure

--- TaskList.rb
+++ TaskList.rb
@@ -1,31 +1,44 @@
-require 'rspec'
-
 class Tasklist 
-  attr_accessor :task, :datetime_start, :datetime_finish
-  
+  attr_accessor :task, :task_status, :task_list
+
   def initialize
-    @time_start = Time.now
+    @task_list = []
+    @task = []
+    @task_status = { :active => "Task In Progress", :inactive => "Task Completed" }
   end
   
-  def task_add
+  def task_create(task_description)
+      @task = @task_status[:active], task_description, Time.now.strftime("%D %R")
+      @task_list << @task
+    end
   end
-  
-  def task_finish
-  end 
   
   def display_all_tasks
+    @task_list.each do |status, description, time|
+      puts "#{status} : #{description}\n\sadded at #{time}"
   end
   
+  def task_remove
+  end 
+  
+  def task_update
+  end
+  
+  def task_delete
+  end
+
 end
 
 weekend = Tasklist.new
-weekend.task_add
-weekend.task_finished
+weekend.task_create("Sleep in")
+weekend.task_create("Party down")
 weekend.display_all_tasks
 
-describe TaskList do
-  #it should have tasks
-  #it should mark when tasks are finished
-  #it should have a datetime when tasks begin
-  #it should have a datetime when tasks are completed
+=begin
+describe Tasklist do
+  it should have tasks
+  it should mark when tasks are completed
+  it should have a datetime when tasks begin
+  it should have a datetime when tasks are completed
 end
+=end
