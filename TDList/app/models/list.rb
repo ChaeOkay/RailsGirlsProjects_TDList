421da0ee4185101387a7cfdce121e86d3d48d6ed
cokeefe <chaemobile@gmail.com>
2013-06-26 16:41:16 -0400

    file cleanup

--- TDList/app/models/list.rb
+++
@@ -1,6 +0,0 @@
-class List < ActiveRecord::Base
-  attr_accessible :name, :list_status
-
-	has_many :tasks, dependent: :destroy
-
-end
--- TDList/spec/helpers/home_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe HomeHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-RSpec.configure do |config|
-	config.include Capybara::DSL
-end
