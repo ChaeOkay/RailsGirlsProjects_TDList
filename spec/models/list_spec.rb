29f73fedb73c068174d7e6d6692e85e13e85e235
cokeefe <chaemobile@gmail.com>
2013-07-08 15:48:35 -0400

    add association tests

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
 describe List do
-  let (:list) { List.new(name: 'Books', list_status: 'In Progress') }
-  cradle_to_cradle = Task.create(description: 'Cradle to Cradle', task_status: 'In Progress', list_id: 1)
-  rabbit_run = Task.create(description: 'Rabbit Run', task_status: 'In Progress', list_id: 1)
 
-  it 'should have a name' do
-    list.name.should == 'Books'
+  it { should have_many(:tasks) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:list_status) }
+
+  describe 'creating a new list' do
+    let (:list) { List.new(name: 'Books', list_status: 'In Progress') }
+
+    it 'should have a name' do
+      list.name.should == 'Books'
+    end
+
+    it 'is not valid if it does not have a name' do
+      list.name = nil
+      list.should_not be_valid
+    end
+
+    it 'should be an instance of List' do
+      list.should be_an_instance_of List
+    end
   end
 
-  it 'is not valid if it does not have a name' do
-    list.name = nil
-    list.should_not be_valid
-  end
-
-  it 'should be an instance of List' do
-    list.should be_an_instance_of List
-  end
-
-  it 'should have tasks' do
-    list.tasks.should == 2
-  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,18 +2,23 @@ require 'spec_helper'
 
 describe Task do
 
-  let (:task) { Task.new(description: 'Life of Pi', task_status: 'In Progress', list_id: 1) }
+  it { should belong_to(:list) }
 
-  it 'should have a description' do
-    task.description.should == 'Life of Pi'
+  describe 'create a new task' do
+    let (:task) { Task.new(description: 'Life of Pi', task_status: 'In Progress', list_id: 1) }
+
+    it 'should have a description' do
+      task.description.should == 'Life of Pi'
+    end
+
+    it 'is not valid if it does not have a task_status' do
+      task.task_status = nil
+      task.should_not be_valid
+    end
+
+    it 'should be an instance of Task' do
+      task.should be_an_instance_of Task
+    end
   end
 
-  it 'is not valid if it does not have a task_status' do
-    task.task_status = nil
-    task.should_not be_valid
-  end
-
-  it 'should be an instance of Task' do
-    task.should be_an_instance_of Task
-  end
 end
